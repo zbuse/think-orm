ac532f568e4dc23cf1fcf674822fe1a0c4fb8b1a
thinkphp <thinkphp@qq.com>
2025-05-23 12:59:57 +0800

    改进

--- src/model/concern/Attribute.php
+++ src/model/concern/Attribute.php
@@ -560,7 +560,7 @@ trait Attribute
             return $value;
         }
 
-        if (!array_key_exists($name, $this->getFields())) {
+        if (!array_key_exists($name, $this->getData()) && !array_key_exists($name, $this->getFields())) {
             // 动态获取关联数据
             $value = $this->getRelationData($name) ?: null;
         } else {
