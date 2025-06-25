a5dc090eca571db0fff2e7c7c33da427eb72db03
thinkphp <thinkphp@qq.com>
2025-06-25 22:25:34 +0800

    增加lazy方法 惰性加载数据 关联查询统一使用lazy方法优化查询

--- src/db/Query.php
+++ src/db/Query.php
@@ -536,6 +536,10 @@ class Query extends BaseQuery
      */
     public function chunk(int $count, callable $callback, string | array | null $column = null, string $order = 'asc'): bool
     {
+        if ($count < 1) {
+            throw new Exception('The chunk size should be at least 1');
+        }
+
         $options = $this->getOptions();
         $column  = $column ?: $this->getPk();
 
@@ -560,9 +564,13 @@ class Query extends BaseQuery
 
         $resultSet = $query->order($column, $order)->select();
 
-        while (count($resultSet) > 0) {
+        while (true) {
             if (false === call_user_func($callback, $resultSet)) {
                 return false;
+            }
+
+            if (count($resultSet) < $count) {
+                break;
             }
 
             if (isset($times)) {
@@ -581,5 +589,45 @@ class Query extends BaseQuery
         }
 
         return true;
+    }
+
+    /**
+     * 惰性分批遍历数据
+     * @param int         $count   每批处理的数量
+     * @param string|null $column  分批处理的字段名
+     * @param string      $order   字段排序 
+     * @return \Generator
+     */
+    public function lazy(int $count = 1000, ?string $column = null, string $order = 'desc')
+    {
+        if ($count < 1) {
+            throw new Exception('The chunk size should be at least 1');
+        }
+
+        $limit   = (int)$this->getOption('limit', 0);
+        $column  = $column ?: $this->getPk();
+        $this->order($column, $order)->limit($count > $limit ? $limit : $count);
+
+        $options   = $this->getOptions();
+        $bind      = $this->bind;
+        $times     = 0;
+        $resultSet = $this->options($options)->select();
+
+        while (true) {
+            foreach ($resultSet as $item) {
+                yield $item;
+                $lastId = $item[$column];
+                $times++;
+            }
+
+            if (count($resultSet) < $count || ($limit && $times >= $limit)) {
+                break;
+            }
+
+            $resultSet = $this->options($options)
+                ->where($column, 'asc' == strtolower($order) ? '>' : '<', $lastId)
+                ->bind($bind)
+                ->select();
+        };
     }
 }
